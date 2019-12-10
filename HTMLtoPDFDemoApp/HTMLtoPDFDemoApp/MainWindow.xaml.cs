8011c3a87551a7b32e075735f333457218e10a40
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-12-10 06:22:48 -0800

    Added

--- HTMLtoPDFDemoApp/HTMLtoPDFDemoApp/MainWindow.xaml.cs
+++ HTMLtoPDFDemoApp/HTMLtoPDFDemoApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace HTMLtoPDFDemoApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
