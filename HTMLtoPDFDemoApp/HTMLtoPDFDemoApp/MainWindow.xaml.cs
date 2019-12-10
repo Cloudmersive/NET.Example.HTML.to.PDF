028e757a75d27510aca484b129aec1f4ff454a60
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-12-10 06:25:13 -0800

    Added

--- HTMLtoPDFDemoApp/HTMLtoPDFDemoApp/MainWindow.xaml.cs
+++ HTMLtoPDFDemoApp/HTMLtoPDFDemoApp/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Cloudmersive.APIClient.NET.DocumentAndDataConvert.Api;
+using Cloudmersive.APIClient.NET.DocumentAndDataConvert.Client;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +30,17 @@ namespace HTMLtoPDFDemoApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Configure API key authorization: Apikey
+            Configuration.Default.AddApiKey("Apikey", "YOUR_API_KEY");
 
+
+
+            var apiInstance = new ConvertDocumentApi();
+            var inputFile = new System.IO.FileStream(txtSource.Text, System.IO.FileMode.Open); // System.IO.Stream | Input file to perform the operation on.
+
+            byte[] result = apiInstance.ConvertDocumentHtmlToPdf(inputFile);
+
+            File.WriteAllBytes(txtOutput.Text, result);
         }
     }
 }
